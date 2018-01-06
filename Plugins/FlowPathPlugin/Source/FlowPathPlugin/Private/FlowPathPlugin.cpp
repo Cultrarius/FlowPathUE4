06d1e48a7931570520cae0308607969c65ecd7f6
Michael Galetzka <galetzka.michael@gmail.com>
2018-01-06 02:26:58 +0100

    Added some debug stuff

--- Plugins/FlowPathPlugin/Source/FlowPathPlugin/Private/FlowPathPlugin.cpp
+++ Plugins/FlowPathPlugin/Source/FlowPathPlugin/Private/FlowPathPlugin.cpp
@@ -1,12 +1,106 @@
 // Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
 
 #include "FlowPathPlugin.h"
+#include "flow/FlowPath.h"
+#include "flow/WaveMarcher.h"
 
 #define LOCTEXT_NAMESPACE "FFlowPathPluginModule"
+
+using namespace flow;
 
 void FFlowPathPluginModule::StartupModule()
 {
 	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+
+    FlowPath path(4);
+    TArray<uint8> tile11 = {
+        1, 1, 1, 1,
+        2, 4, 255, 1,
+        1, 2, 1, 255,
+        1, 1, 1, 1
+    };
+    TArray<uint8> tile21 = {
+        255, 1, 255, 1,
+        255, 1, 255, 1,
+        255, 1, 255, 255,
+        255, 1, 1, 1
+    };
+    TArray<uint8> tile12 = {
+        1, 1, 1, 1,
+        1, 1, 1, 1,
+        1, 255, 255, 255,
+        1, 1, 1, 1
+    };
+    TArray<uint8> tile22 = {
+        255, 1, 1, 1,
+        255, 1, 1, 1,
+        255, 255, 1, 1,
+        1, 1, 1, 1
+    };
+    path.updateMapTile(1, 1, tile11);
+    path.updateMapTile(1, 2, tile12);
+    path.updateMapTile(2, 1, tile21);
+    path.updateMapTile(2, 2, tile22);
+
+    tile21 = {
+        255, 1, 255, 1,
+        3, 1, 255, 1,
+        255, 1, 255, 255,
+        255, 1, 1, 1
+    };
+    path.updateMapTile(2, 1, tile21);
+    UE_LOG(LogExec, Warning, TEXT("---------------------"));
+
+    PathSearchResult result = path.findDirectPath(FIntPoint(1, 0), FIntPoint(3, 3));
+    UE_LOG(LogExec, Warning, TEXT("Success: %d, Cost: %d"), result.success, result.pathCost);
+    for (FIntPoint p : result.waypoints) {
+        UE_LOG(LogExec, Warning, TEXT("Waypoint %d, %d:"), p.X, p.Y);
+    }
+    UE_LOG(LogExec, Warning, TEXT("---------------------"));
+
+    TilePoint searchStart = { FIntPoint(1, 1), FIntPoint(1, 0) };
+    TilePoint searchEnd = { FIntPoint(2, 2), FIntPoint(1, 3) };
+    PortalSearchResult portalResult = path.findPortalPath(searchStart, searchEnd);
+    UE_LOG(LogExec, Warning, TEXT("Success: %d, Cost: %d"), portalResult.success, portalResult.pathCost);
+    UE_LOG(LogExec, Warning, TEXT("Portal search [start]"));
+    for (int i = portalResult.waypoints.Num() - 1; i >= 0; i--) {
+        const Portal* p = portalResult.waypoints[i];
+        UE_LOG(LogExec, Warning, TEXT("Portal waypoint %d, %d on tile %d, %d:"), p->center.X, p->center.Y, p->parentTile->getCoordinates().X, p->parentTile->getCoordinates().Y);
+    }
+    UE_LOG(LogExec, Warning, TEXT("Portal search [end]"));
+    UE_LOG(LogExec, Warning, TEXT("---------------------"));
+
+
+    TArray<uint8> bigTile = {
+        2, 2, 2, 2, 2, 2, 2, 2, 2, 4,
+        2, 2, 2, 2, 2, 2, 2, 2, 2, 4,
+        2, 2, 2, 2, 2, 2, 2, 2, 2, 4,
+        2, 2, 2, 2, 2, 2, 2, 2, 3, 3,
+        2, 2, 2, 2, 2, 2, 2, 2, 3, 3,
+        2, 2, 2, 2, 2, 2, 2, 2, 3, 3,
+        2, 2, 2, 2, 2, 2, 2, 2, 3, 3,
+        2, 2, 2, 255, 2, 2, 2, 2, 2, 3,
+        2, 2, 2, 255, 2, 2, 2, 2, 2, 3,
+        2, 2, 2, 255, 2, 2, 2, 2, 2, 2,
+    };
+    TArray<FIntPoint> targets;
+    targets.Emplace(0, 9);
+    //targets.Emplace(1, 9);
+    //targets.Emplace(2, 9);
+    TArray<EikonalCellValue> eikonal = CreateEikonalSurface(bigTile, targets);
+
+    for (int32 i = 0; i < 10; i++) {
+        EikonalCellValue* v = &eikonal[i * 10];
+        UE_LOG(LogExec, Warning, TEXT("%f %f %f %f %f %f %f %f %f %f"), v[0].cellValue, v[1].cellValue, v[2].cellValue, v[3].cellValue, v[4].cellValue, v[5].cellValue, v[6].cellValue, v[7].cellValue, v[8].cellValue, v[9].cellValue);
+    }
+    UE_LOG(LogExec, Warning, TEXT("---------------------"));
+    for (int32 i = 0; i < 10; i++) {
+        EikonalCellValue* v = &eikonal[i * 10];
+        UE_LOG(LogExec, Warning, TEXT("%d %d %d %d %d %d %d %d %d %d"), v[0].directionLookupIndex, v[1].directionLookupIndex, v[2].directionLookupIndex, v[3].directionLookupIndex, v[4].directionLookupIndex,
+            v[5].directionLookupIndex, v[6].directionLookupIndex, v[7].directionLookupIndex, v[8].directionLookupIndex, v[9].directionLookupIndex);
+    }
+
+    UE_LOG(LogExec, Warning, TEXT("Meh"));
 }
 
 void FFlowPathPluginModule::ShutdownModule()
